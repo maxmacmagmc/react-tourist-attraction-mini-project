5118a931eaa7aad27e8c7cda7e6883e985278b07
Wutt Saeng <wuttforwork@gmail.com>
2025-03-30 22:33:32 +0700

    feat: Add Card for travel searchBar and recommendation Tag

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
-  return <div className="App text-xl bg-red-500">hello</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
