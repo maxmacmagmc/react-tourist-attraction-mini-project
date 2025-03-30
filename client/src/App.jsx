492b5aa082bce10394b24766e5293d40f9c8c9d5
Wutt Saeng <wuttforwork@gmail.com>
2025-03-30 19:30:07 +0700

    Set up Project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">{/* Start coding here */}</div>;
+  return <div className="App text-xl bg-red-500">hello</div>;
 }
 
 export default App;
